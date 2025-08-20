05ae60ac50a39baa304fcb233fd20101eb67b34c
harigovind11 <harigovindsreejith007@gmail.com>
2025-08-20 23:44:54 +0530

    Adds fruit data assets
    -Introduces ScriptableObject assets for various fruit types, a prefab template with physics components, and core gameplay logic to support fruit merging mechanics.
    -Lays the foundation for fruit progression and interaction in the game.

---
+++ Assets/Scripts/Data/FruitData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FruitData", menuName = "Berrydox/Fruit Data")]
+public class FruitData : ScriptableObject
+{
+    public int fruitIndex;
+    public int points;
+    public Sprite fruitSprite;
+    public FruitData nextFruitData;
+}
